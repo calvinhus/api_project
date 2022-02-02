478eceeb9a348c3de7103417c0fa27dcc7d136b6
calvinhus <gjpcalvinho@gmail.com>
2022-02-02 18:06:08 +0000

    meal method done

---
+++ main.py
@@ -0,0 +1,10 @@
+# import libraries
+import pandas as pd
+import api_requests as our_api
+
+print(f"\n\t\t\t---WELCOME TO DATING APP PROJECT---\n")
+#duration = input("\nEnter duration: ")
+# date_type = input(
+#    "\nEnter the type of date you want:\n\nRomantic - [1]\nSoft - [2]\nBold - [3]\nXtra Bold - [4]\n\nYour choice:")
+
+our_api.get_meal("Vegetarian")
