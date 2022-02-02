54cdaa45fe40bcef59c2230a34f6303f77e60913
calvinhus <gjpcalvinho@gmail.com>
2022-02-02 16:16:02 +0000

    added movies dataset

---
+++ api_requests.py
@@ -0,0 +1,8 @@
+import requests
+
+joao = 'hello im joao'
+
+
+def chuck_norris():
+    url = ''
+    response = requests.get(url)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # import libraries
+import pandas as pd
+import api_requests as our_api
+
+print(f"WELCOME TO DATING APP PROJECT")
+#duration = input("enter duration: ")
+#date_type = input("enter type: ")
+movies = pd.read_csv('data/imdb_top_1000.csv')
+print(movies['Genre'].unique())
