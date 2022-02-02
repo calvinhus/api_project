43f35def28c274543437ced15277a2641c6c97c3
calvinhus <gjpcalvinho@gmail.com>
2022-02-02 19:15:43 +0000

    dot env

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # import libraries
 import api_requests as our_api
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+smt = os.environ.get("API_KEY")
+print(smt)
 print(f"\n\t\t\t---WELCOME TO DATING APP PROJECT---\n")
 #duration = input("\nEnter duration: ")
 # date_type = input(
 #    "\nEnter the type of date you want:\n\nRomantic - [1]\nSoft - [2]\nBold - [3]\nXtra Bold - [4]\n\nYour choice:")
 
-our_api.get_meal("Vegetarian")
+# our_api.get_meal("Vegetarian")
